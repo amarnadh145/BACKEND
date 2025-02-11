9269ae36f3f95c40bb3b3f98a0fef7985ab2009e
Amarnadh Reddy Mettu <amarnadhusa@gmail.com>
2025-02-10 19:48:59 -0500

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,7 +5,7 @@ router.post('/add-product/:firmId', productController.addProduct)
 router.get('/:firmId/products', productController.getProductByFirm)
 router.get("/uploads/:imageName", (req, res) => {
     const imageName = req.params.imageName
-    res.headers('Content-Type', 'image/jpeg')
+    res.header('Content-Type', 'image/jpeg')
     res.sendFile(path.join(__dirname, '..', 'uploads', imageName))
 })
 router.delete('/:productId', productController.deleteProductById)
